039a1db07d743fe6f6698726365d26fecfec2955
ChoiAhHyun <cah1113@naver.com>
2020-02-18 03:06:47 +0900

    Rename MainActivity -> MemoListActivity MemoListActivity UI - Add RecyclerView, Toolbar, ImageButton

--- app/src/main/java/com/line/android/memoapp/view/MemoListActivity.java
+++ app/src/main/java/com/line/android/memoapp/view/MemoListActivity.java
@@ -1,14 +1,16 @@
-package com.line.android.memoapp;
+package com.line.android.memoapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.line.android.memoapp.R;
+
+public class MemoListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_memo_list);
     }
 }
