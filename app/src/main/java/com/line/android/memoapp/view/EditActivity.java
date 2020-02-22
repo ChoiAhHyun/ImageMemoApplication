20ba0af6afd4f4e90862794e69a35f9e3e929ef0
ChoiAhHyun <cah1113@naver.com>
2020-02-23 04:57:47 +0900

    EditActivity - ActionBar 완성

--- app/src/main/java/com/line/android/memoapp/view/EditActivity.java
+++ app/src/main/java/com/line/android/memoapp/view/EditActivity.java
@@ -3,6 +3,8 @@ package com.line.android.memoapp.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.line.android.memoapp.R;
 
@@ -12,5 +14,28 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed(); //TODO override해서 dialog로 확인
+                return true;
+            case R.id.upload:
+                return true;
+            case R.id.save:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
