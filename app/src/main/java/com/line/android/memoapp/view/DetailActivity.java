d25ca252464333cdd40ea05390723108934646bf
ChoiAhHyun <cah1113@naver.com>
2020-02-23 04:18:48 +0900

    DetailActivity - 삭제 제외한 작업 완료

--- app/src/main/java/com/line/android/memoapp/view/DetailActivity.java
+++ app/src/main/java/com/line/android/memoapp/view/DetailActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,30 +20,64 @@ import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private String title, content;
+    private ArrayList image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Intent intent = getIntent();
+        title = intent.getStringExtra("title");
+        content = intent.getStringExtra("content");
+        image = intent.getParcelableArrayListExtra("image");
 
         TextView tv_title = findViewById(R.id.tv_title);
-        tv_title.setText(intent.getStringExtra("title"));
+        tv_title.setText(title);
         TextView tv_content = findViewById(R.id.tv_content);
-        tv_content.setText(intent.getStringExtra("content"));
+        tv_content.setText(content);
 
         RecyclerView recyclerView = findViewById(R.id.pager);
-        ArrayList list = intent.getParcelableArrayListExtra("image");
-        if (list == null){
+        if (image == null){
             recyclerView.setVisibility(View.GONE);
         } else {
             recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-            ImageAdapter adapter = new ImageAdapter(this, list);
+            ImageAdapter adapter = new ImageAdapter(this, image);
             recyclerView.setAdapter(adapter);
 
             PagerSnapHelper snapHelper = new PagerSnapHelper();
             snapHelper.attachToRecyclerView(recyclerView);
             recyclerView.addItemDecoration(new CirclePagerIndicatorDecoration());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.delete:
+                //TODO 데이터베이스 연결한 후
+                return true;
+            case R.id.edit:
+                Intent intent = new Intent(this, EditActivity.class);
+                intent.putExtra("title", title);
+                intent.putExtra("content", content);
+                intent.putExtra("image", image);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
